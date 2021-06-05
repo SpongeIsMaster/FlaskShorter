42f00bd491e5571b51728f60ddd5e7c681071abb
sponge <spongeyt161@gmail.com>
2021-06-05 23:20:16 +0200

    finish

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
+from os import link
 from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__)
 
-end_link = ""
+short_url = ''
+links = []
+id = 0
 
 @app.route("/", methods=["POST", "GET"])
-def elo():
-    return render_template("towrite.html")
-
-@app.route("/home", methods=["POST", "GET"])
 def home():
-    global end_link
+    global link, links, id, short_url
     if request.method == "POST":
-        link = request.form["long-url"]
-        end_link = link
+        
+        id = id + 1
+        links.append((id, request.form["long-url"]))
+        short_url = f"http://127.0.0.1:5000/shortener/{id}"
         return redirect(url_for("succes"))
     else:
         return render_template("towrite.html")
 
 @app.route("/succes")
 def succes():
-    global end_link
-    print(end_link)
-    return render_template("succes.html")
-    
+    global short_url
+    print(links)
+    return render_template("succes.html", link = short_url, id = id)
+
+@app.route("/shortener/<id>")
+def findAress():
+    return redirect(url_list(links[id - 1][1]))
+
 if __name__ == "__main__":
     app.run(debug=True)
